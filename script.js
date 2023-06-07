cd4f5809c498494a395f6fbc968cf9383883f598
hkagei <k_dawg_1022@hotmail.com>
2023-06-07 17:12:28 -0600

    get started with css and js files

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1280;
+    canvas.height = 720;
+
+    class Player {
+        constructor(){
+
+        }
+    }
+
+    class Game {
+        constructor(canvas){
+            this.canvas = canvas;
+            this.width = this.canvas.width;
+            
+        }
+    }
+
+    function animate(){
+
+    }
+})
