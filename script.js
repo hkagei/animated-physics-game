99d375e09f560dfb2e175a55a8d74af0a85336cd
hkagei <k_dawg_1022@hotmail.com>
2023-06-08 18:05:42 -0600

    implement player movement features

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ window.addEventListener('load', function(){
     canvas.width = 1280;
     canvas.height = 720;
 
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
+
     class Player {
         constructor(game){
-            this.game = game;            
+            this.game = game;
+            this.collisionX = this.game.width * 0.5;
+            this.collisionY = this.game.height * 0.5;
+            this.collisionRadius = 30;
         }
         draw(context){
             context.beginPath();
-            context.arc(100, 100, 50, 0, Math.PI * 2);
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+            context.save();
+            context.globalAlpha = 0.5;
             context.fill();
+            context.restore();
+            context.stroke();
         }
     }
 
